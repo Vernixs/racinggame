a84ac449529ef110c045c857baf35a6ab91eabbf
Vernixs <enecelis2021@elam.co.uk>
2023-03-31 21:49:00 +0100

    made progress on mutliplayer game

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "GameServer";
+
+            Server.Start(50, 29650);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Racing game/Assets/Scripts/Server/UImanager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UImanager : MonoBehaviour
+{
+    public static UImanager instance;
+    public GameObject startMenu;
+    public InputField usernameField;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object: ");
+            Destroy(this);
+        }
+    }
+
+    public void ConnectToServer()
+    {
+        startMenu.SetActive(false);
+        usernameField.interactable= false;
+        Client.instance.ConnectToServer();
+        SceneManager.LoadScene("Main Menu");
+    }
+}
